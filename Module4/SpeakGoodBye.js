f8c689a19959c3ba637d414e34e651c1700b441d
ayoub farah <youblicious@gmail.com>
2017-09-18 04:26:38 +0100

    removing some unwanted code

--- Module4/SpeakGoodBye.js
+++ Module4/SpeakGoodBye.js
@@ -20,8 +20,7 @@ var speakWord = "Good Bye";
 byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
-window.byeSpeaker = byeSpeaker;
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
-// xxxx.xxxx = byeSpeaker;
+window.byeSpeaker = byeSpeaker;
 })(window);
